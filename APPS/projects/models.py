f8fee3a8dcc9a31771d36eb764dba7f310528493
NikodemRoy <nikodem.roy@gmail.com>
2022-06-15 12:28:09 +0200

    commit before project model migration

--- APPS/projects/models.py
+++ APPS/projects/models.py
@@ -6,32 +6,34 @@ from parler.models import TranslatableModel, TranslatedFields
 
 class Mainproject(TranslatableModel):
     translations = TranslatedFields(
-        project_name = models.CharField(max_length=2550),
-        project_shortdescription = models.CharField(max_length=2550),
-        skill_fulldescription = models.CharField(max_length=2550),
+        project_name = models.CharField(max_length=255),
+        project_shortdescription = models.CharField(max_length=2550, blank=True),
+        skill_fulldescription = models.TextField(max_length=25500, blank=True),
+        github_link = models.CharField(max_length=255, blank=True),
     )
 
     # profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
     stack = models.ManyToManyField('stack', blank=True)
     is_public = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='project_cover', default=None)
-    image = models.ImageField(upload_to='project_image', default=None)
+    cover = models.ImageField(upload_to='project_cover', blank=True)
+    image = models.ImageField(upload_to='project_image', blank=True)
 
     def __str__(self):
         return self.project_name
 
 class projects(TranslatableModel):
     translations = TranslatedFields(
-        project_name = models.CharField(max_length=2550),
-        project_shortdescription = models.CharField(max_length=2550),
-        skill_fulldescription = models.CharField(max_length=2550),
+        project_name = models.CharField(max_length=255),
+        project_shortdescription = models.CharField(max_length=2550, blank=True),
+        skill_fulldescription = models.TextField(max_length=25500, blank=True),
+        github_link = models.CharField(max_length=255, blank=True),
     )
 
     # profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
     stack = models.ManyToManyField('stack', blank=True)
     is_public = models.BooleanField(default=False)
     
-    image = models.ImageField(upload_to='project_image', default=None)
+    image = models.ImageField(upload_to='project_image', blank=True)
 
     def __str__(self):
         return self.project_name
