e1720f15adb3e383ba58166cedeba019cfc6d281
NikodemRoy <nikodem.roy@gmail.com>
2022-06-19 11:16:50 +0200

    working comments

--- APPS/posts/views.py
+++ APPS/posts/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from APPS.blog.models import BlogPost
+from APPS.comments.models import Comment
 
 from django.shortcuts import get_object_or_404
+from django.http import Http404
 
 # Create your views here.
 
 def post(request, post_slug):
     post_detail = get_object_or_404(BlogPost, translations__slug = post_slug)
-    print(post_slug)
-    context = {'post_detail':post_detail}
+   
+   
+    # comments = Comment.objects.filter(project__translations__slug=post_slug)
+    if '/en/' in request.path:
+        comments = Comment.objects.filter(project=post_detail, text_pl='')
+
+    elif '/pl/' in request.path:  
+        comments = Comment.objects.filter(project=post_detail, text_en='')
+    else: 
+        raise Http404("Internal page error") 
+    print(comments)
+    context = {
+        'post_detail':post_detail,
+        'comments':comments,
+        }
     return render(request, 'posts/single_post.html', context )
