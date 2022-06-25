f428cb9cf8ec79671aeda91dc06700ff0d84a810
NikodemRoy <nikodem.roy@gmail.com>
2022-06-25 09:06:51 +0200

    added inline in profile model

--- APPS/about_me/admin.py
+++ APPS/about_me/admin.py
@@ -2,12 +2,24 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from parler.admin import TranslatableAdmin
+from parler.admin import TranslatableAdmin, TranslatableInlineModelAdmin, TranslatableStackedInline
 
 from .models import Mainskill, Profile, Shortdescription, Skill
 
+class ShortdescriptionInline(TranslatableStackedInline):
+    model = Shortdescription
+    extra = 0
+
+class SkillInline(TranslatableStackedInline):
+    model = Skill
+    extra = 0
+
+class ProfileAdmin(TranslatableAdmin):
+    inlines = [ShortdescriptionInline, SkillInline]
+
+
 admin.site.register(Shortdescription, TranslatableAdmin)
-admin.site.register(Profile, TranslatableAdmin)
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Mainskill, TranslatableAdmin)
 admin.site.register(Skill, TranslatableAdmin)
 
