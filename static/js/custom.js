cb93aa3d3fb9cb385277b62aa63a2d0d9a0b6c7c
NikodemRoy <nikodem.roy@gmail.com>
2022-07-01 17:26:19 +0200

    changed navbar- working

--- static/js/custom.js
+++ static/js/custom.js
@@ -30,6 +30,49 @@ for (i = 0; i < linkToggle.length; i++) {
 }
 
 
+function getScrollMaxY() {
+    "use strict";
+    var innerh = window.innerHeight || ebody.clientHeight,
+      yWithScroll = 0;
+  
+    if (window.innerHeight && window.scrollMaxY) {
+      // Firefox 
+      yWithScroll = window.innerHeight + window.scrollMaxY;
+    } else if (document.body.scrollHeight > document.body.offsetHeight) {
+      // all but Explorer Mac 
+      yWithScroll = document.body.scrollHeight;
+    } else {
+      // works in Explorer 6 Strict, Mozilla (not FF) and Safari 
+      yWithScroll = document.body.offsetHeight;
+    }
+    return yWithScroll - innerh;
+  }
+  
+  function setEqualHeight() {
+    let scrollAblepx = getScrollMaxY();
+    // var maxscroll = window.scrollMaxY
+    let innerWidth = window.innerWidth
+    if(innerWidth<768){
+      if (scrollAblepx < 105) {
+        document.querySelector(".footer").classList.add("no_scroll");
+      } else {
+        document.querySelector(".footer").classList.remove("no_scroll");
+      }
+  
+    }else{
+      if (scrollAblepx < 5) {
+        document.querySelector(".footer").classList.add("no_scroll");
+      } else {
+        document.querySelector(".footer").classList.remove("no_scroll");
+      }
+  
+    }
+    
+  }
+  setEqualHeight();
+  window.onresize = setEqualHeight;
+
+
 
 // var button = document.getElementById('save_btn');
 
@@ -49,4 +92,3 @@ for (i = 0; i < linkToggle.length; i++) {
 //         document.querySelector('form').submit();
 //     }
 // });
-
